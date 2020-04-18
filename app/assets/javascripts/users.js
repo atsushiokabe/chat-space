85070edb2c605ab13a56e6b540c46bced88fa3ac
Atsushiokabe <atsushiokabe@icloud.com>
2020-04-18 19:10:21 +0900

    create HTML and append it
    テンプレートリテラルを使用し返り値としてhtmlを返す。
    doneメソッドで条件分岐で受け取った配列型の引数一つ一つに対して処理ができるようにする。
    appendメソッドでuser-search-resuldに情報を追加付与
    エラーの場合の処理もする。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,4 +1,22 @@
 $(function(){
+  function finduser(user){
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div>
+    `;
+    console.log(html)
+    // $("#user-search-result").append(html);
+  }
+  function nouser() {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">ユーザーが見つかりません</p>
+      </div>
+    `;
+    $("#user-search-result").append(html);
+  }
   $("#user-search-field").on('keyup',function(){
     var input = $("#user-search-field").val();
     $.ajax({
@@ -7,11 +25,21 @@ $(function(){
       data: { keyword: input },
       dataType: 'json'
     })
-      .done(function(){
-        console.log("succeed");
+      .done(function(users){
+        $("#user-search-result").empty();
+
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            finduser(user);
+          });
+        } else if (input.length == 0) {
+          return false;
+        } else {
+          nouser();
+        }
       })
       .fail(function(){
-        console.log("failed");
+        alert("通信エラーです。ユーザーが表示できません。");
       })
   })
 })
