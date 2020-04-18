1b117f849014811bc3b2cde8944db54008ba8e5c
Atsushiokabe <atsushiokabe@icloud.com>
2020-04-18 15:57:02 +0900

    set ajax when events occur
    users_controllerの、indexアクションにリクエストの送信先を'/users'に設定する
    曖昧な検索にも対応できるようにLIKE ? メソッドを使用し、検索結果に現在のユーザーが出ないようにする。

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,17 @@
 $(function(){
   $("#user-search-field").on('keyup',function(){
     var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+      .done(function(){
+        console.log("succeed");
+      })
+      .fail(function(){
+        console.log("failed");
+      })
   })
 })
--- app/views/users/index.json.builder
+++
@@ -1,4 +0,0 @@
-json.array! @user do |user|
-  json.id user.id
-  json.name user_name
-end
