c514f615b32cf70525d984fcba2dab4bf279f06f
Atsushiokabe <atsushiokabe@icloud.com>
2020-04-20 00:32:55 +0900

    created index.json.jbuilder
    jsonj形式でデータを送るときにidもデータとして渡すようにjson.id @message.idを記載。
    最新のメッセージのidを取得するために$('.message:last').data$(message.id)を変数last_message_idに代入。
    代入した数値を$.ajaxにて送信。
    buildHTMLにて非同期作成するhtmlにもidを付与するためにmessageにdata-message-id=${message.id}を記載
    setInterval()関数を用いて7秒に一度reloadMessagesを読み込むように設定。reloadMessagesでは最新の投稿のidからデータを取り出し、html型のデータとしてappendメソッドを使用。

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,27 +1,27 @@
 $(function(){
-     function buildHTML(message){
-      if (message.image){
-        var html =
-         `<div class="message">
-            <div class="message__info"
-              <div class=message__info__name">
-                ${message.user_name}
-              </div>
-              <div class="message__info__date">
-                ${message.created_at}
-              </div>
+  function buildHTML(message){
+    if (message.image){
+      var html =
+        `<div class="message" data-message-id=${message.id}>
+          <div class="message__info">
+            <div class="message__info__name">
+              ${message.user.name}
             </div>
-            <div class="message__text">
-              <p class="lower-message__content">
-                ${message.content}
-              </p>
+            <div class="message__info__date">
+              ${message.created_at}
             </div>
-            <img src=${message.image}>
-          </div>`
-        return html;
-      } else{
-        var html =
-        `<div class="message">
+          </div>
+          <div class="message__text">
+            <p class="lower-message__content">
+              ${message.content}
+            </p>
+          </div>
+          <img src=${message.image}>
+        </div>`
+      return html;
+    } else{
+      var html =
+        `<div class="message" data-message-id=${message.id}>
           <div class="message__info">
             <div class=message__info__name">
               ${message.user_name}
@@ -36,9 +36,29 @@ $(function(){
             </p>
           </div>
         </div>`
-        return html;
-      };
-    }
+      return html;
+    };
+  }
+    var reloadMessages = function() {
+      var last_message_id = $('.message:last').data("message.id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+      })
+      .fail(function() {
+        alert('error');
+      });
+    };
+
   $("#new_message").on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
@@ -52,14 +72,17 @@ $(function(){
       contentType: false,
     })
     .done(function(data){
+      console.log(data)
       var html = buildHTML(data);
       $('.messages').append(html);
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
       $('form')[0].reset();
       $('.submit-btn').prop('disabled', false);
+      console.log(html)
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
   });
-  })
+  });
+  setInterval(reloadMessages, 7000);
 });
